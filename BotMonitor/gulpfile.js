fa698b8820662fb39863f71fc00c30d15c92fab3
Drew Kestell <drew.kestell@gmail.com>
2018-06-22 16:03:11 -0500

    WIP

--- BotMonitor/gulpfile.js
+++ BotMonitor/gulpfile.js
@@ -1,15 +1,23 @@
 ﻿/// <binding Clean='minify:css' ProjectOpened='watch' />
-"use strict";
+'use strict';
 
 var gulp = require('gulp'),
     compileSass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
     rename = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    sassLint = require('gulp-sass-lint');
 
 var webRoot = './wwwroot/';
 
-gulp.task('compile:sass', function () {
+gulp.task('lint:sass', function () {
+    return gulp.src('./Styles/styles.scss')
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError());
+});
+
+gulp.task('compile:sass', ['lint:sass'], function () {
     return gulp.src('./Styles/styles.scss')
         .pipe(compileSass())
         .pipe(gulp.dest(webRoot + 'css'));
