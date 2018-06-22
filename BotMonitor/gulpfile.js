5d4f073ecd623e20c1c9fd30d1d9479b4215f9d5
Drew Kestell <drew.kestell@gmail.com>
2018-06-22 14:18:18 -0500

    WIP

---
+++ BotMonitor/gulpfile.js
@@ -0,0 +1,31 @@
+﻿/// <binding Clean='minify:css' ProjectOpened='watch' />
+"use strict";
+
+var gulp = require('gulp'),
+    compileSass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps');
+
+var webRoot = './wwwroot/';
+
+gulp.task('compile:sass', function () {
+    return gulp.src('./Styles/styles.scss')
+        .pipe(compileSass())
+        .pipe(gulp.dest(webRoot + 'css'));
+});
+
+gulp.task('minify:css', ['compile:sass'], function () {
+    gulp.src([webRoot + 'css/*.css', '!' + webRoot + 'css/*.min.css'])
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS({ compatibility: '*' }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(webRoot + 'css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./Styles/styles.scss', ['minify:css']);
+});
